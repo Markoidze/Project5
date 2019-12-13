743c783dbfffab72676887632ff1a9861c884479
Markoidze <manucharmarkoidze@gmail.com>
2019-12-12 19:06:02 -0500

    Don!!

--- src/MyMa/Hyundai.java
+++ src/MyMa/Hyundai.java
@@ -3,42 +3,53 @@ package MyMa;
 import java.text.DecimalFormat;
 
 public class Hyundai extends carLease implements carInterface {
+    public Hyundai(String name, String whichModel, int age, int salary, int totalCost, int downPayment, int termofLease, int leaseFee, double taxRate, double moneyFactor) {
+        super(name, whichModel, age, salary, totalCost, downPayment, termofLease, leaseFee, taxRate, moneyFactor);
+    }
 
+    @Override
+    public String leaseMonthlyCalculator() {
+        double result = calculateDepreciation() + calculateInterest() + taxes();
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formattedResult = df.format(result);
+        return formattedResult;
+    }
+
+    public boolean isAligable(){
+        if(getSalary() > hyundaiMinIncome){
+            return true;
+        }
+        return false;
+    }
+
+    public String totalCalculator() {
+        double result = (Double.valueOf(leaseMonthlyCalculator()) * termofLease) + documentationFees;
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formattedResult = df.format(result);
+        return formattedResult;
+    }
+}
     /*
-
       get the connection with carLease and carInterface
         after the connection you should have constructor ,  leaseMonthlyCalculator method and isAligable method
-
     */
 
     /*
              leaseMonthlyCalculator   --->> return type is String
-
             calculation is --->> calculateDepreciation + calculateInterest + taxes (these methods are coming from the carLease method)
-
             hint:  after the calculation result result will be 2.11222211 you need to change result as a 2.11
-
     */
 
      /*
          isAligable
-
             if the salary is more then hyundaiMinIncome then true else false
-
      */
 
 
       /*
         method name is totalCalculator
-
         return type is String
-
         calculation :  (leaseMonthlyCalculator * termofLease) + documentationFees
-
         Format the calculation result
-
         hint:  after the calculation result result will be 2.11222211 you need to change result as a 2.11
-
      */
-
-}
--- src/MyMa/carLease.java
+++ src/MyMa/carLease.java
@@ -2,63 +2,63 @@ package MyMa;
 
 public abstract class carLease {
 
+    private String name , whichModel;
+    private int age ,salary , totalCost , downPayment  , leaseFee , depractionAmount;
+    int termofLease;
+    private double taxRate , moneyFactor , interestAmount;
     /*
-        Create 2 string   (name , whichModel)
-
-        7 int (age ,salary , totalCost , downPayment , termofLease , leaseFee , depractionAmount )
-
-        3  double (taxRate , moneyFactor , interestAmount)
-
-     */
-
-
-    /*
-
         Create a constructor
-
         All the variables are should be as a parameter except (depractionAmount and interestAmount)
-
         depractionAmount = calculateDepreciation method (you will create this in this class)
-
         interestAmount = calculateInterest method (you will create this in this class)
-
      */
 
-
-/*
- Create a method
-
-        calculateDepreciation
-
-        return type should be int
-
-        calculation is :
-
-        total cost minus down payment divide by term of lease
-
- */
-
+    public carLease(String name, String whichModel, int age, int salary, int totalCost, int downPayment, int termofLease, int leaseFee, double taxRate, double moneyFactor) {
+        this.name = name;
+        this.whichModel = whichModel;
+        this.age = age;
+        this.salary = salary;
+        this.totalCost = totalCost;
+        this.downPayment = downPayment;
+        this.termofLease = termofLease;
+        this.leaseFee = leaseFee;
+        this.taxRate = taxRate;
+        this.moneyFactor = moneyFactor;
+        depractionAmount = calculateDepreciation();
+        interestAmount = calculateInterest();
+    }
+    /*
+     Create a method
+            calculateDepreciation
+            return type should be int
+            calculation is :
+            total cost minus down payment divide by term of lease
+     */
+    int calculateDepreciation(){
+        return  (totalCost - downPayment) / termofLease;
+    }
     /*
        calculateInterest
-
                 return type should  be double
-
                 calculation is : total cost plus down payment multiply by money factor
-
      */
-
+    double calculateInterest(){
+        return (totalCost + downPayment) * moneyFactor;  //TODO: check this
+    }
     /*
-
         taxes
            return type should be double
-
             calculation is :
             depractionAmount + interestAmount * taxRate
-
      */
-
+    double taxes(){
+        return  depractionAmount + interestAmount * taxRate;
+    }
     /*
      getter for the (getSalary)
      */
 
+    public int getSalary() {
+        return salary;
+    }
 }
